cfa25f1aac947d4c9d6b50ae445b1105a46cee54
Rychard Antony Pereira de Arruda <143468418+RychardFacul@users.noreply.github.com>
2024-09-13 23:49:33 +0000

    init

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      {/** HEADER */}
+      {/** HERO */}
+      {/** PESQUISA/FILTROS */}
+      {/**  */}
+      {/** CARD DOS FILMES */}
+      {/** FOOTER */}
+    </div>
+  );
+}
