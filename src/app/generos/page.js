80009068f39528553b169583e94427f2d0b460a6
0Fafnir <eduardosantana127@gmail.com>
2024-10-03 19:29:03 -0400

    Alteracao no designer das paginas

--- src/app/generos/page.js
+++ src/app/generos/page.js
@@ -7,6 +7,7 @@ import GeneroTag from "@/components/genero_tag";
 import Header from "@/components/layouts/header";
 import Footer from "@/components/layouts/footer";
 import * as GenerosManager from "@/lib/server_side_props/generos";
+import background from "../../imagens/background.webp";
 
 export default function Generos() {
   const [generos, setGeneros] = useState([]);
@@ -63,73 +64,75 @@ export default function Generos() {
   return (
     <>
       <Header small={true} />
-      <div className="container flex justify-center mx-auto h-[80vh] items-center">
-        <main className="relative p-10 bg-gray-100 shadow-lg rounded-xl w-[50vw]">
-          <div className="flex items-center justify-center h-52">
-            <div className="flex items-center relief justify-center mb-28 bg-blue-100 rounded-full p-[100px]">
-              <div className="flex items-center justify-center w-5 h-5">
-                <h1 className="mt-5 mb-4 font-serif text-3xl font-bold text-center">Gêneros</h1>
+      <div  style={{ backgroundImage: `url(${background.src})` }}>
+        <div className="container flex justify-center mx-auto h-[90vh] items-center">
+          <main className="relative p-10 bg-gray-950 shadow-lg rounded-xl w-[50vw] bg-opacity-65">
+            <div className="flex items-center justify-center h-52">
+              <div className="flex items-center relief justify-center mb-28 bg-blue-100 rounded-full p-[100px]">
+                <div className="flex items-center justify-center w-5 h-5">
+                  <h1 className="mt-5 mb-4 font-serif text-3xl font-bold text-center">Gêneros</h1>
+                </div>
               </div>
             </div>
-          </div>
-          
-          {currentGenero.id && (
-            <FaTrash
-              onClick={() => handleDelete(currentGenero.id)}
-              className="absolute w-6 h-6 text-red-500 transition-colors duration-200 cursor-pointer top-4 right-4 hover:text-red-700"
-            />
-          )}
+            
+            {currentGenero.id && (
+              <FaTrash
+                onClick={() => handleDelete(currentGenero.id)}
+                className="absolute w-6 h-6 text-red-500 transition-colors duration-200 cursor-pointer top-4 right-4 hover:text-red-700"
+              />
+            )}
 
-          <div className="flex items-center justify-between mb-6">
-            <div className="flex flex-wrap flex-1 gap-4 overflow-y-auto max-h-[93px]">
-              {generos.map((g, i) => (
-                <GeneroTag
-                  key={`genero-${i}`}
-                  genero={g}
-                  onClick={() => setCurrentGenero(g)}
-                  className={`cursor-pointer transition-all duration-200 ${
-                    currentGenero.id === g.id ? "bg-blue-500 text-white" : "bg-gray-200 hover:bg-blue-200 text-gray-800"
-                  } rounded-lg shadow-sm`}
-                />
-              ))}
+            <div className="flex items-center justify-between mb-6">
+              <div className="flex flex-wrap flex-1 gap-4 overflow-y-auto max-h-[93px]">
+                {generos.map((g, i) => (
+                  <GeneroTag
+                    key={`genero-${i}`}
+                    genero={g}
+                    onClick={() => setCurrentGenero(g)}
+                    className={`cursor-pointer transition-all duration-200 ${
+                      currentGenero.id === g.id ? "bg-blue-500 text-white" : "bg-gray-200 hover:bg-blue-200 text-gray-800"
+                    } rounded-lg shadow-sm`}
+                  />
+                ))}
+              </div>
+              <AiTwotonePlusCircle
+                onClick={handleAdd}
+                className="w-5 h-5 ml-4 text-blue-500 transition-colors duration-200 cursor-pointer hover:text-blue-700"
+              />
             </div>
-            <AiTwotonePlusCircle
-              onClick={handleAdd}
-              className="w-5 h-5 ml-4 text-blue-500 transition-colors duration-200 cursor-pointer hover:text-blue-700"
-            />
-          </div>
 
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div className="flex gap-5">
-              <label className="flex-1 block">
-                <span className="text-gray-700">Nome</span>
-                <input
-                  type="text"
-                  name="name"
-                  className="w-full p-2 border rounded-lg focus:ring-2 focus:ring-blue-300"
-                  value={currentGenero.name}
-                  onChange={(e) => setCurrentGenero({ ...currentGenero, name: e.target.value })}
-                />
-              </label>
-              <label className="flex-1 block">
-                <span className="text-gray-700">Descrição</span>
-                <input
-                  type="text"
-                  name="description"
-                  className="w-full p-2 border rounded-lg focus:ring-2 focus:ring-blue-300"
-                  value={currentGenero.description}
-                  onChange={(e) => setCurrentGenero({ ...currentGenero, description: e.target.value })}
-                />
-              </label>
-            </div>
-            <button
-              type="submit"
-              className="w-full p-2 text-white transition-all duration-200 bg-blue-500 rounded-lg hover:bg-blue-600"
-            >
-              Salvar
-            </button>
-          </form>
-        </main>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="flex gap-5">
+                <label className="flex-1 block">
+                  <span className="text-gray-100">Nome</span>
+                  <input
+                    type="text"
+                    name="name"
+                    className="w-full p-2 border rounded-lg focus:ring-2 focus:ring-blue-300"
+                    value={currentGenero.name}
+                    onChange={(e) => setCurrentGenero({ ...currentGenero, name: e.target.value })}
+                  />
+                </label>
+                <label className="flex-1 block">
+                  <span className="text-gray-100">Descrição</span>
+                  <input
+                    type="text"
+                    name="description"
+                    className="w-full p-2 border rounded-lg focus:ring-2 focus:ring-blue-300"
+                    value={currentGenero.description}
+                    onChange={(e) => setCurrentGenero({ ...currentGenero, description: e.target.value })}
+                  />
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="w-full p-2 text-white transition-all duration-200 bg-blue-500 rounded-lg hover:bg-blue-600"
+              >
+                Salvar
+              </button>
+            </form>
+          </main>
+        </div>
       </div>
       <Footer />
     </>
